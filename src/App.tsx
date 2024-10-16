632eaea4efcfbd743ebcc527dd02178ddb78a58f
cybermage <cybermage69@gmail.com>
2024-10-16 15:54:36 +0800

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ window.Buffer = Buffer;
 function App() {
   const [input1, setInput1] = useState("");
   const [input2, setInput2] = useState("");
+  const [input3, setInput3] = useState("");
 
   const output1 = useMemo(() => {
     if (!input1) {
@@ -20,8 +21,13 @@ function App() {
     return toOpcode(input2);
   }, [input2]);
 
+  const output3 = useMemo(() => {
+    return u.sha256(input3).toUpperCase();
+  }, [input3]);
+
   return (
-    <div style={{ padding: "20px" }}>
+    <div style={{ padding: "0 20px" }}>
+      <h3>Sign message verify</h3>
       <div>Input:</div>
       <textarea
         value={input1}
@@ -30,7 +36,8 @@ function App() {
       ></textarea>
       <div>Output:</div>
       <div style={{ wordBreak: "break-all" }}>{output1}</div>
-      <div style={{ marginTop: "50px" }}>Input:</div>
+      <h3 style={{ marginTop: "50px" }}>Script analysis</h3>
+      <div>Input:</div>
       <textarea
         value={input2}
         onChange={(e) => setInput2(e.target.value)}
@@ -43,6 +50,15 @@ function App() {
           __html: output2 as any,
         }}
       ></div>
+      <h3 style={{ marginTop: "50px" }}>SHA256</h3>
+      <div>Input:</div>
+      <textarea
+        value={input3}
+        onChange={(e) => setInput3(e.target.value)}
+        style={{ width: "100%", height: "200px" }}
+      ></textarea>
+      <div>Output:</div>
+      <div style={{ wordBreak: "break-all" }}>{output3}</div>
     </div>
   );
 }
