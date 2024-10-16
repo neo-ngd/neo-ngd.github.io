6fa43996c64585d492069bbe04111f102efd0745
cybermage <cybermage69@gmail.com>
2024-10-16 18:21:04 +0800

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useMemo, useState } from "react";
 import { Buffer } from "buffer";
-import toOpcode from "./utils";
+import toOpcode from "./utils/toOpcode";
 import { u } from "@cityofzion/neon-js";
+import { isJSON } from "./utils";
 
 window.Buffer = Buffer;
 
 function App() {
   const [input1, setInput1] = useState("");
   const [input2, setInput2] = useState("");
-  const [input3, setInput3] = useState("");
 
   const output1 = useMemo(() => {
     if (!input1) {
@@ -18,47 +18,52 @@ function App() {
   }, [input1]);
 
   const output2 = useMemo(() => {
-    return toOpcode(input2);
+    let input = input2;
+    if (isJSON(input)) {
+      input = JSON.parse(input)?.script ?? "";
+    }
+    return toOpcode(input);
   }, [input2]);
 
   const output3 = useMemo(() => {
-    return u.sha256(input3).toUpperCase();
-  }, [input3]);
+    let input = input2;
+    if (!input) {
+      return "";
+    }
+    if (isJSON(input)) {
+      input = JSON.parse(input)?.script ?? "";
+    }
+    input = u.base642hex(input);
+    return u.sha256(input).toUpperCase();
+  }, [input2]);
 
   return (
     <div style={{ padding: "0 20px" }}>
       <h3>Sign message verify</h3>
-      <div>Input:</div>
+      <h5>Input:</h5>
       <textarea
         value={input1}
         onChange={(e) => setInput1(e.target.value)}
         style={{ width: "100%", height: "200px" }}
       ></textarea>
-      <div>Output:</div>
+      <h5>Output:</h5>
       <div style={{ wordBreak: "break-all" }}>{output1}</div>
       <h3 style={{ marginTop: "50px" }}>Script analysis</h3>
-      <div>Input:</div>
+      <h5>Input(JSON/Base64):</h5>
       <textarea
         value={input2}
         onChange={(e) => setInput2(e.target.value)}
         style={{ width: "100%", height: "200px" }}
       ></textarea>
-      <div>Output:</div>
+      <h5>Output(to SHA256):</h5>
+      <div style={{ wordBreak: "break-all" }}>{output3}</div>
+      <h5>Output(to Smart contract script analysis):</h5>
       <div
         style={{ wordBreak: "break-all" }}
         dangerouslySetInnerHTML={{
           __html: output2 as any,
         }}
       ></div>
-      <h3 style={{ marginTop: "50px" }}>SHA256</h3>
-      <div>Input:</div>
-      <textarea
-        value={input3}
-        onChange={(e) => setInput3(e.target.value)}
-        style={{ width: "100%", height: "200px" }}
-      ></textarea>
-      <div>Output:</div>
-      <div style={{ wordBreak: "break-all" }}>{output3}</div>
     </div>
   );
 }
---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+export function isJSON(str) {
+  try {
+    const parsed = JSON.parse(str);
+    return typeof parsed === "object" && parsed !== null;
+  } catch (e) {
+    return false;
+  }
+}
